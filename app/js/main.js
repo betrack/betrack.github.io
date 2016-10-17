01623bfc25557cfc794d2d226ad77d95dbd7edf2
Sergio Fruto <sergio.fruto@auth0.com>
2016-10-17 09:32:19 -0300

    fixed issues with the navigation in desktop + added new files for scroll animations

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,26 @@
 $( document ).ready(function() {
+  //init WOW for scroll animations
+  new WOW().init();
+
   // open sidebar navigation(tablet & desktop only)
   $( ".sidebar-toggle" ).click(function() {
     $('.sidebar-navigation').toggleClass( "open" );
+  });
+
+  // smooth scroll for links in nav
+  $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          var navbarHeight = $('.navbar-header').height();
+          $('html, body').animate({
+            scrollTop: target.offset().top - navbarHeight
+          }, 1000);
+          return false;
+        }
+      }
+    });
   });
 });
